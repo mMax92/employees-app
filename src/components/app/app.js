703708efad27f41bb02cff17522f317dd4c40223
MMax <mr.moshnyatskiy@gmail.com>
2023-06-11 17:04:07 +0300

    Commit 2.0. Refactored employees-list  with props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+    const data = [
+        {name: "John S.", salary: 2200, increase: true},
+        {name: "Sean T.", salary: 900, increase: false},
+        {name: "Emma W.", salary: 1500, increase: true},
+    ];
+
     return (
         <div className="app">
             <AppInfo/>
@@ -16,7 +23,7 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployeesList/>
+            <EmployeesList data={data}/>
             <EmployeesAddForm/>
         </div>
     );
