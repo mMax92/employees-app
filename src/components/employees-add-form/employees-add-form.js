47e46f4d8d3c5856e7cb6c4a3f2d54619bcffd81
MMax <mr.moshnyatskiy@gmail.com>
2023-06-17 16:38:43 +0300

    Commit 4.0. Added function for deleting of the employee by clicking on trash bucket. Added function for submitting/adding in the list the new employee

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -8,6 +8,7 @@ class EmployeesAddForm extends Component {
             name: '',
             salary: ''
         }
+       this.maxId = 4;
     }
 
     onValueChange = (e) => {
@@ -16,6 +17,16 @@ class EmployeesAddForm extends Component {
         })
     } 
 
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+
+        })
+    }
+
     render() {
         const {name, salary} = this.state;
 
@@ -23,7 +34,8 @@ class EmployeesAddForm extends Component {
             <div className="app-add-form">
                 <h3>Add new employee</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit={this.onSubmit}>
                     <input 
                         type="text"
                         className="form-control new-post-label"
